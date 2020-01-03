0d92db4ff5999e78226b8f80b582785ab4ad39b2
Naman Arora <xavier.arora@gmail.com>
2020-01-03 13:40:03 +0530

    init kernel code

---
+++ kernel.c
@@ -0,0 +1,31 @@
+
+void main()
+{
+
+    char *video_mem = (char *)0xb8000; // address offset of the first text cell of the video memeory.
+    int n = 5;
+
+    char *name = "Naman";
+    int i = 0;
+
+    *video_mem = 'K';
+    video_mem += 2;
+
+    *video_mem = 'A';
+    video_mem += 2;
+
+    *video_mem = 'N';
+    video_mem += 2;
+
+    *video_mem = 'A';
+    video_mem += 2;
+
+    *video_mem = 'O';
+    video_mem += 2;
+
+    *video_mem = 'S';
+    video_mem += 2;
+
+    *video_mem = ' ';
+    video_mem += 2;
+}
