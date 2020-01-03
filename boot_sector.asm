b9e1663fc929c8d3bac03e71cc11f0bf89036cf8
Naman Arora <xavier.arora@gmail.com>
2020-01-03 13:39:45 +0530

    load kernel from disk

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,15 +1,14 @@
-; Bootloader
-
 [org 0x7c00]
 ; sets assembler location counter to 0x7C00 (address where BIOS loads this program)
 ; An alternative to the above approach will be to set Data segment to 0x7c0.
 
+KERNEL_OFFSET equ 0x1000    ; memeory offset at which we will load the kernel from the disk.
 
 [bits 16]
 
 jmp main
 
-; RReal mode includes
+; Real mode includes
 %include "print_utils.asm"
 %include "disk_utils.asm"
 
@@ -19,11 +18,27 @@ main:
 
     mov bx, in_real_mode_msg
     call print
+
+    call load_kernel
+
     call switch_to_protected_mode
 
     jmp $
 
+load_kernel:
+    mov bx, load_kernel_msg
+    call print
 
+    mov bx, KERNEL_OFFSET   ; Params for disk load. We load 15 sectors (dh) from drive dl to bx offset in the memory.
+    mov dh, 15
+    mov dl, [BOOT_DRIVE]
+
+    call disk_load
+
+    mov bx, success
+    call print
+
+    ret
 
 switch_to_protected_mode:
     cli ; disable interrupts
@@ -61,12 +76,17 @@ BEGIN_PM:
     mov ebx, in_protected_mode_msg
     call print_string_pm
 
+    call KERNEL_OFFSET
+
     jmp $
 
 ; data
+
 BOOT_DRIVE: db 0
 in_real_mode_msg: db 'In real Mode', 0
 in_protected_mode_msg: db 'In protected Mode', 0
+load_kernel_msg: db 'Loading kernel from disk', 0
+success: db 'Loaded Kernel', 0
 
 ; Magic number and offset
 
