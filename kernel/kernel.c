1e45c76559bfc26c419f2d24421231a35129c4fc
Naman Arora <xavier.arora@gmail.com>
2020-01-15 20:48:14 +0530

    initialize IDT on execution

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,9 +1,13 @@
 #include "../drivers/screen.h"
+#include "../cpu/idt.h"
 
 void main()
 {
     clear_screen();
+    idt_init();
 
-    char *message = "Hello! Welcome to KANA OS!\0";
+    char *message = "Hello! Welcome to KANA OS!\n\0";
     print(message);
+
+    __asm__ volatile("int $0x1F");
 }
